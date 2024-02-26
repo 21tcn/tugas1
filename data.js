3ca9014468a636e7c02118ed7eb6c9fd6ab9cf84
Taqy <115392037+21tcn@users.noreply.github.com>
2024-02-26 19:04:51 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get('username');
+const npm = urlParams.get('npm');
+const gender = urlParams.get('gender');
+const birthday = urlParams.get('birthday');
 
 document.getElementById("usernameDisplay").textContent = username;
+document.getElementById("npmDisplay").textContent = npm;
+document.getElementById("genderDisplay").textContent = gender;
+document.getElementById("birthdayDisplay").textContent = birthday;
+
